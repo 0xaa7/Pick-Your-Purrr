7d7ab26cd113a08620a3c6db0a8f1efcca81cedc
Aamsher <hpandian008@gmail.com>
2024-01-13 22:35:25 +0530

    updated video card

--- src/components/video/videoCard/VideoCard.jsx
+++ src/components/video/videoCard/VideoCard.jsx
@@ -1,15 +1,5 @@
-import { useEffect, useRef } from "react";
+import {  useRef } from "react";
 import { useScroll, motion, useTransform } from "framer-motion";
-
-const debounce = (func, delay) => {
-  let timeoutId;
-  return (...args) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
-};
 
 const VideoCard = ({ data }) => {
   const ref = useRef(null);
@@ -18,17 +8,11 @@ const VideoCard = ({ data }) => {
   const scale1 = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacity1 = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
 
-  const debouncedScroll = debounce(() => {
-  }, 20);
-
-  useEffect(() => {
-    debouncedScroll();
-  }, [scrollYProgress, debouncedScroll]);
-
+ 
   return (
     <motion.div key={data.id} style={{ scale: scale1, opacity: opacity1 }} className="video-card">
       <div className="card" ref={ref}>
-        <video src={data.video} />
+        <video src={data.video} autoPlay loop muted />
       </div>
       <div className="content">
         <h1>{data.name}</h1>

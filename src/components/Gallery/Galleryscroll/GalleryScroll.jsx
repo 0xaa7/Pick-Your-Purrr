fc2f019f572a19a4ed741ce3b22acbae1452ccea
Aamsher <hpandian008@gmail.com>
2024-02-07 20:53:11 +0530

    tried memo

--- src/components/Gallery/Galleryscroll/GalleryScroll.jsx
+++ src/components/Gallery/Galleryscroll/GalleryScroll.jsx
@@ -1,11 +1,10 @@
 
 
-import { useEffect, useRef } from "react";
-import "./galleryscroll.scss"
-import { useTransform, useScroll, motion } from "framer-motion"
-import Lenis from '@studio-freight/lenis'
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-
+import { useTransform, useScroll, motion } from "framer-motion";
+import Lenis from '@studio-freight/lenis';
+import "./galleryscroll.scss";
 
 const images = [
   "cat1.jpeg",
@@ -29,40 +28,38 @@ const images = [
   
 ]
 
-export default function GalleryScroll () {
-
+const GalleryScroll = React.memo(() => {
+  
   const container = useRef(null);
-  const { scrollYProgress } = useScroll(
-    {
-      target:container,
-      offset:['start end' , 'end start']
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start end', 'end start']
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [0, 500]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, -200]);
+  const y3 = useTransform(scrollYProgress, [0, 2], [0, 200]);
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, -230]);
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
     }
-  )
-const y = useTransform(scrollYProgress, [0,1],[0,500])
- const y2 = useTransform(scrollYProgress, [0,1],[0,-200])
- const y3 = useTransform(scrollYProgress, [0,2],[0,200])
- const y4 = useTransform(scrollYProgress, [0,1],[0,-230])
 
-
-useEffect(()=>{
-  const lenis = new Lenis()
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
-},[])
+    requestAnimationFrame(raf);
+  }, []);
 
   return (
-    <div className="gallery-scroll" >
+    <div className="gallery-scroll">
       <div className="top-space"></div>
       <div ref={container} className="gallery">
-        <Column images={[images[0],images[1],images[2],images[3],images[6]]} y={y} />
-        <Column images={[images[4],images[5],images[6],images[7],images[12]]} y={y2} />
-        <Column images={[images[6],images[7],images[8],images[2],images[13]]} y={y3} />
-        <Column className="col" images={[images[9],images[10],images[14],images[15],images[17],]} y={y4} />
+        <Column images={[images[0], images[1], images[2], images[3], images[6]]} y={y} />
+        <Column images={[images[4], images[5], images[6], images[7], images[12]]} y={y2} />
+        <Column images={[images[6], images[7], images[8], images[2], images[13]]} y={y3} />
+        <Column className="col" images={[images[9], images[10], images[14], images[15], images[17],]} y={y4} />
         <span>
           <Link to="/gallerypage">
             <button>Gallery</button>
@@ -71,22 +68,25 @@ requestAnimationFrame(raf)
       </div>
       <div className="bottom-space"></div>
     </div>
-  )
-}
+  );
+});
 
+GalleryScroll.displayName = "GalleryScroll";
 
-
-const Column = ({images, y=0}) => {
-  return(
-    <motion.div style={{y}} className="column">
-      {
-        images.map((src, index)=>{
-          return <div key={index} className="imageContainer">
+const Column = React.memo(({ images, y = 0 }) => {
+  return (
+    <motion.div style={{ y }} className="column">
+      {images.map((src, index) => {
+        return (
+          <div key={index} className="imageContainer">
             <img src={`/Images/${src}`} alt="image" />
-            
           </div>
-        })
-      }
+        );
+      })}
     </motion.div>
-  )
-}
+  );
+});
+
+Column.displayName = "Column";
+
+export default GalleryScroll;
